2669692356aa12bf81aa0cf007a956b216ae80d5
Ben McGahee <ben.mcgahee@gmail.com>
2016-04-14 20:02:23 -0400

    Update todo.php

--- todo.php
+++ todo.php
@@ -1,12 +1,16 @@
 <?php
+//Author: Ben McGahee
+//Title: Write to File
+//Date: 4/13/2016
+//Purpose: This script creates and write text to a text file and displays the current time when the file was saved.
 
 $file = "todolist.txt";
 
-$openFile = fopen($file, 'w')
+$openFile = fopen($file, 'w') //create a text file called todolist.txt
 or
-die("Unable to create " .$file);
+die("Unable to create " .$file); //file cannot be created due to insufficient memory
 
-$fileText = <<<_END
+$fileText = <<<_END 
 File Taxes
 Mow the Lawn
 Buy Groceries
@@ -14,16 +18,16 @@ Feed the Dog
 Do Laundry
 _END;
 
-fwrite($openFile, $fileText)
+fwrite($openFile, $fileText) //write this text to the file
 or
 die("Unable to write to " .$openFile);
 
-fclose($openFile);
+fclose($openFile); //close the file
 
 $easternTimeZone = "America/New_York";
 timeStamp($easternTimeZone, $file);
 
-function timeStamp($timeZone, $file)
+function timeStamp($timeZone, $file) //timeStamp records the date and time when file was saved
 {
    date_default_timezone_set($timeZone);
    $currentTime = date("l F jS, Y @ g:ia", time());
